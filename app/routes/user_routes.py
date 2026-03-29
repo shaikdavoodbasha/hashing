74f06a0a2d88f373b57c5670d22577d4b012124e
shaikdavoodbasha <shannushaik080@gmail.com>
2026-03-29 13:01:00 +0530

    hashed

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,37 +1,37 @@
 
-# from fastapi import  Depends, HTTPException,APIRouter
-# from sqlmodel import select
-# from typing import Annotated
-# from app.dependancies import SessionDep,get_current_user
-# from app.schemas.user_schemas import CreateUser,Token
-# from app.models.user_model import User
-# from app.auth import hash_password,verify_password,create_access_token
-# from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import  Depends, HTTPException,APIRouter
+from sqlmodel import select
+from typing import Annotated
+from app.dependancies import SessionDep,get_current_user
+from app.schemas.user_schemas import CreateUser,Token
+from app.models.user_model import User
+from app.auth import hash_password,verify_password,create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
-# router = APIRouter()
+router = APIRouter()
 
-# @router.post("/register")
-# def register(session:SessionDep,user_data:CreateUser):
-#     if session.exec(select(User).where(User.email == user_data.email)).first():
-#         raise HTTPException(status_code=400,detail="Email is already registered")
-#     hash_pwd = hash_password(user_data.password)
-#     user = User(email = user_data.email,name = user_data.name,hashed_password = hash_pwd)
-#     session.add(user)
-#     session.commit()
-#     session.refresh(user)
-#     return user
+@router.post("/register")
+def register(session:SessionDep,user_data:CreateUser):
+    if session.exec(select(User).where(User.email == user_data.email)).first():
+        raise HTTPException(status_code=400,detail="Email is already registered")
+    hash_pwd = hash_password(user_data.password)
+    user = User(email = user_data.email,name = user_data.name,hashed_password = hash_pwd)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
 
-# @router.post("/login",response_model=Token)
-# def login(session:SessionDep,form_data:Annotated[OAuth2PasswordRequestForm,Depends()]):
-#     user = session.exec(select(User).where(User.email == form_data.username)).first()
+@router.post("/login",response_model=Token)
+def login(session:SessionDep,form_data:Annotated[OAuth2PasswordRequestForm,Depends()]):
+    user = session.exec(select(User).where(User.email == form_data.username)).first()
 
-#     pwd = verify_password(form_data.password,user.hashed_password)
+    pwd = verify_password(form_data.password,user.hashed_password)
 
-#     if not user or not pwd:
-#          raise HTTPException(status_code=400,detail="Invalid Credentials")
-#     token = create_access_token(data = {"sub":user.email})
-#     return {"access_token":token,"token_type":"bearer"}
+    if not user or not pwd:
+         raise HTTPException(status_code=400,detail="Invalid Credentials")
+    token = create_access_token(data = {"sub":user.email})
+    return {"access_token":token,"token_type":"bearer"}
 
-# @router.get("/profile")
-# def profile(current_user:Annotated[User,Depends(get_current_user)]):
-#     return {"name":current_user,"email":current_user.email}
+@router.get("/profile")
+def profile(current_user:Annotated[User,Depends(get_current_user)]):
+    return {"name":current_user,"email":current_user.email}
